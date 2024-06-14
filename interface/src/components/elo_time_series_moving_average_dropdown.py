0a17b79bdf9e4aed03ba2f9e0b3bcae574d4a828
Luís Felipe Ramos Ferreira <lframos_ferreira@outlook.com>
2024-06-14 18:39:11 -0300

    media movel adicionado

--- interface/src/components/elo_time_series_moving_average_dropdown.py
+++ interface/src/components/elo_time_series_moving_average_dropdown.py
@@ -10,17 +10,38 @@ from settings import REVERSED_CLUBS_DICT
 
 def render(df_dict: dict[str, pd.DataFrame]) -> None:
     @callback(
-        Output("elo-moving-average-evolution-chart", "figure"),
-        Input("ticker-moving-average", "value"),
+        [
+            Output(
+                "elo-moving-average-evolution-chart", "figure", allow_duplicate=True
+            ),
+            Output("ticker-moving-average", "value"),
+        ],
+        Input("period-selector", "value"),
+        prevent_initial_call=True,
     )
-    def display_time_series(ticker):
+    def apply_moving_average(period):
+        # clear plot and dropdown values
+        fig = go.Figure()
+        fig.update_layout(xaxis_title="Ano", yaxis_title="Elo")
+        return fig, []
+
+    @callback(
+        Output("elo-moving-average-evolution-chart", "figure"),
+        [Input("ticker-moving-average", "value"), Input("period-selector", "value")],
+    )
+    def display_time_series(ticker, period):
         if len(ticker) == 0:
             raise exceptions.PreventUpdate
         fig = go.Figure()
         for club in ticker:
             club_df = df_dict[REVERSED_CLUBS_DICT[club]]
             fig.add_trace(
-                go.Scatter(x=club_df["From"], y=club_df["Elo"], mode="lines", name=club)
+                go.Scatter(
+                    x=club_df["From"],
+                    y=club_df["Elo"].rolling(period).mean(),
+                    mode="lines",
+                    name=club,
+                )
             )
         fig.update_layout(xaxis_title="Ano", yaxis_title="Elo")
         return fig
--- interface/src/components/elo_time_series_moving_average_plot.py
+++ interface/src/components/elo_time_series_moving_average_plot.py
@@ -13,6 +13,22 @@ def render(df_dict: dict[str, pd.DataFrame]) -> html.Div:
         [
             html.H4("Média móvel de variação de Elo ao longo dos anos"),
             dcc.Graph(id="elo-moving-average-evolution-chart"),
+            html.P("Selecione um valor para o comprimento da média móvel"),
+            dcc.RadioItems(
+                id="period-selector",
+                options=[
+                    {"label": "5", "value": 5},
+                    {"label": "10", "value": 10},
+                    {"label": "15", "value": 15},
+                    {"label": "20", "value": 20},
+                    {"label": "25", "value": 25},
+                    {"label": "30", "value": 30},
+                ],
+                value=5,
+                labelStyle={"display": "inline-block", "margin-right": "10px"},
+                inputStyle={"margin-right": "5px"},
+                style={"display": "inline-block", "text-align": "center"},
+            ),
             html.P("Selecione um clube:"),
             dcc.Dropdown(
                 id="ticker-moving-average",
